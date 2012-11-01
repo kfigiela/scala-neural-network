0cdfa0c0662a5d3dd646d5dc5c644d64c28ee57b
Kamil Figiela <kamil.figiela@gmail.com>
2012-11-01 20:24:09 +0100

    Argument name correction

--- src/main/scala/nnetworks/Network.scala
+++ src/main/scala/nnetworks/Network.scala
@@ -1,7 +1,7 @@
 package nnetworks
 
-case class Layer(activation: (Double) => Double, weights: List[List[Double]]) {
-  def apply(input: List[Double]): List[Double] = weights map ((neuron) => activation(((1.0 :: input), neuron).zipped.map(_*_).reduce(_+_)))
+case class Layer(activation: (Double) => Double, neurons: List[List[Double]]) {
+  def apply(input: List[Double]): List[Double] = neurons map ((neuron) => activation(((1.0 :: input), neuron).zipped.map(_*_).reduce(_+_)))
 }
 
 case class Network(layers: Layer*){
